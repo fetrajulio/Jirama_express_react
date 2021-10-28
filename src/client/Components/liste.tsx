993f03d7109a15ff68df1e25c46a0fdbdad7922b
fetrajulio <fetrajulio@gmail.com>
2021-10-28 20:04:44 +0200

    modif 3

--- src/client/Components/liste.tsx
+++ src/client/Components/liste.tsx
@@ -7,9 +7,9 @@ export default class Liste extends React.Component{
                 <div className="card border-primary mb-3">
                     <div className="card-header">Liste Des Clients</div>
                     <div className="card-body">
-                        
+                            
                     </div>
-                    </div>
+                </div>
           </div>
             
         );
