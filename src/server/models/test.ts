36ee25477cd6ba73360748d217f0908ae94cc365
fetrajulio <fetrajulio@gmail.com>
2021-10-29 07:23:44 +0200

    manambotra model test

--- src/server/models/test.ts
+++ src/server/models/test.ts
@@ -1,7 +1,7 @@
 import {ITest} from "../domain/ITest";
 import Database from '../dbConfigs';
 import {Schema} from "mongoose";
-
+//maka base de donne ato am model
 const {mongo: {model}} = Database;
 
 const TestSchema: Schema<ITest> = new Schema<ITest>({text: {type: String, required: true}});
