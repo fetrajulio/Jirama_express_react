24f5ee63ed47e1704b5958b7e03fbc4a4b9d8a9f
fetrajulio <fetrajulio@gmail.com>
2021-10-29 08:42:44 +0200

    liste->modification (2)

--- src/client/Components/liste.tsx
+++ src/client/Components/liste.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 interface IIprops{
     tab:{
         Name:string;
-    listes: {id:number; nom:string; num:string ; prix:string;}[]
+    listes: {id:number; nom:string; num:string ; prix:number;}[]
     }
     
 }
@@ -26,10 +26,10 @@ const Lits = (props: IIprops) =>{
             <div>
                 <a href="#" className="list-group-item list-group-item-action">
                     <div className="row">
-                        <div className="col-2">{idrandom(0,30)}</div>
+                        <div className="col-2">{i.id}</div>
                         <div className="col-4">{i.nom}</div>
-                        <div className="col-3">{i.num}</div>
-                        <div className="col-3">{i.prix}Ar  
+                        <div className="col-3">CT-{i.num}</div>
+                        <div className="col-3">{i.prix}00Ar  
                         
                         <span className="badge bg-primary">Non Payer</span></div>
                     </div>
@@ -52,9 +52,16 @@ export default class Liste extends React.Component{
         const datab ={
             Name:"client555",
             listes:[
-                {id:0, nom:"julio",num:"1235",prix:"54100"},
-                {id:1, nom:"123julio",num:"121211235",prix:"5411321300"},
-                {id:2, nom:"julio1",num:"11321235",prix:"541321100"},
+                {id:0, nom:"julio",num:"TANA5-1235",prix:idrandom(5,3040)},
+                {id:1, nom:"berziky",num:"TANA5-1215",prix:idrandom(5,3940)},
+                {id:2, nom:"bezara",num:"TANA3-1135",prix:idrandom(5,3030)},
+                {id:3, nom:"armel",num:"TANA1-1145",prix:idrandom(5,3040)},
+                {id:4, nom:"meva",num:"TANA4-1136",prix:idrandom(5,3040)},
+                {id:5, nom:"vatosoa",num:"TANA3-1133",prix:idrandom(5,3040)},
+                {id:6, nom:"njara",num:"TANA1-1335",prix:idrandom(5,30400)},
+                {id:7, nom:"njarasoa",num:"TANA1-5635",prix:idrandom(5,304000)},
+                {id:8, nom:"andriamiahaja",num:"TANA3-9235",prix:idrandom(5,30400)},
+                {id:9, nom:"hobitina",num:"TANA1-5425",prix:idrandom(5,30400)},
             ]
         }
         return (
